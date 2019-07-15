f05af278c3a61c8323ba4e6f959ab3bd18f681f1
Hans Enlin <hans.enlin@icloud.com>
2019-07-15 17:35:15 -0400

    add date and time checks

--- server/server.js
+++ server/server.js
@@ -10,6 +10,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 let reservations = [];
 
+let opentime = 10.0;
+let closetime = 20.0;
+
+function checktime(time) {
+	return time >= opentime && (time+1) <=closetime;
+}
+
+function checkday(day) {
+	return day >= 1 && day <= 31;
+}
+function checkmonth(month) {
+	return month >= 1 && month <= 12;
+}
+function checkyear(year) {
+	return year >= 2019;
+}
+
 app.get("/reservations", function(req, res){
 	res.json(reservations);
 });
@@ -19,21 +36,26 @@ app.post('/', (req, res) => {
 
 	let input = req.body.Body.split(" ");
 
-	if (input.length !== 4) {
-		console.log("error: require more inputs");
-		twiml.message('error: require more inputs');
+	if (input.length !== 7 && checktime(input[3]) && checkday(input[4]) && checkday(input[5]) && checkday(input[6])) {
+		console.log("error");
+		twiml.message('error');
 	} else {
 		let reservation = {
 			firstname: input[0],
-			lastname: input[1],
+			lastname: input[1], 
 			partysize: input[2],
-			time: input[3]
+			time: input[3],
+			day: input[4],
+			month: input[5],
+			year: input[6]
 		}
 
 		twiml.message('success');
 
 		reservations.push(reservation);
+
 		console.log(reservations);
+
 	}
 
 	res.writeHead(200, {'Content-Type': 'text/xml'});

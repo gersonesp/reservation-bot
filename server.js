bcc55bf566f8123ec33ddaf37d1ae24a6f1e987e
Gerson Espinal <gerson.espinal@gmail.com>
2019-07-18 17:29:39 -0400

    heroku fix

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-const http = require('http');
-const express = require('express');
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
-const bodyParser = require('body-parser');
+const http = require("http");
+const express = require("express");
+const MessagingResponse = require("twilio").twiml.MessagingResponse;
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
 
 let reservations = [];
 
@@ -14,58 +13,63 @@ let opentime = 10.0;
 let closetime = 20.0;
 
 function checktime(time) {
-	return time >= opentime && (time+1) <=closetime;
+  return time >= opentime && time + 1 <= closetime;
 }
 
 function checkday(day) {
-	return day >= 1 && day <= 31;
+  return day >= 1 && day <= 31;
 }
 function checkmonth(month) {
-	return month >= 1 && month <= 12;
+  return month >= 1 && month <= 12;
 }
 function checkyear(year) {
-	return year >= 2019;
+  return year >= 2019;
 }
 
-app.get("/reservations", function(req, res){
-	res.json(reservations);
+app.get("/reservations", function(req, res) {
+  res.json(reservations);
 });
 
-app.post('/', (req, res) => {
-	const twiml = new MessagingResponse();
+app.post("/", (req, res) => {
+  const twiml = new MessagingResponse();
 
-	let input = req.body.Body.split(" ");
+  let input = req.body.Body.split(" ");
 
-	if (input.length !== 8 && checktime(input[3]) && checkday(input[4]) && checkmonth(input[5]) && checkyear(input[6])) {
-		console.log("error");
-		twiml.message('error');
-	} else {
-		let s = input[6] + "-" + input[5] + "-" + input[4] + input[3] + ":00:00.000Z"
-		let reservation = {
-			firstname: input[0],
-			lastname: input[1], 
-			partysize: input[2],
-			time: input[3],
-			day: input[4],
-			month: input[5],
-			year: input[6],
-			isodate: s
-		}
+  if (
+    input.length !== 8 &&
+    checktime(input[3]) &&
+    checkday(input[4]) &&
+    checkmonth(input[5]) &&
+    checkyear(input[6])
+  ) {
+    console.log("error");
+    twiml.message("error");
+  } else {
+    let s =
+      input[6] + "-" + input[5] + "-" + input[4] + input[3] + ":00:00.000Z";
+    let reservation = {
+      firstname: input[0],
+      lastname: input[1],
+      partysize: input[2],
+      time: input[3],
+      day: input[4],
+      month: input[5],
+      year: input[6],
+      isodate: s
+    };
 
-		twiml.message('success');
+    twiml.message("success");
 
-		reservations.push(reservation);
+    reservations.push(reservation);
 
-		console.log(reservations);
-		reservations.sort((a, b) => (a.isodate > b.isodate) ? 1 : -1);
+    console.log(reservations);
+    reservations.sort((a, b) => (a.isodate > b.isodate ? 1 : -1));
+  }
 
-	}
-
-	res.writeHead(200, {'Content-Type': 'text/xml'});
-	res.end(twiml.toString());
-})
+  res.writeHead(200, { "Content-Type": "text/xml" });
+  res.end(twiml.toString());
+});
 
 http.createServer(app).listen(1337, () => {
-	console.log('express server listening on port 1337')
+  console.log("express server listening on port 1337");
 });
-

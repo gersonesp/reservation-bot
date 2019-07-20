e6e2eca1bee38e42268fff69869edf0f050b0767
Gerson Espinal <gerson.espinal@gmail.com>
2019-07-19 21:06:24 -0400

    fixed heroku header

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,9 @@ import ReservationsList from "./components/reservations/ReservationsList";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <ReservationsList />
-      </header>
+      </div>
     </div>
   );
 }
